37b11eccf0d005c21293821217cc0384bdf729f5
Alex Cahn <alexbcahn@gmail>
2019-04-23 20:45:24 -0500

    Create firebase.js

---
+++ assets/firebase/firebase.js
@@ -0,0 +1,15 @@
+// {/* <script src="https://www.gstatic.com/firebasejs/5.10.0/firebase.js"></script> */}
+/* <script> */
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDHAQNOMMsN4OfyE3V14tdcXZczWHMAahc",
+    authDomain: "flocka-time.firebaseapp.com",
+    databaseURL: "https://flocka-time.firebaseio.com",
+    projectId: "flocka-time",
+    storageBucket: "flocka-time.appspot.com",
+    messagingSenderId: "176647479098"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+// </script>
