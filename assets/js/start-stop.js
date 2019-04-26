5b73e6d8e10bc528fe01bd4ab8b2a36c9226cca1
Alex Cahn <alexbcahn@gmail>
2019-04-25 21:17:21 -0500

    Update start-stop.js

--- assets/js/start-stop.js
+++ assets/js/start-stop.js
@@ -3,6 +3,7 @@ var sessionsRef = firebase.database().ref("sessions");
 sessionsRef.push({
   startedAt: firebase.database.ServerValue.TIMESTAMP
 });
+database.ref("users/" + userId + "/startedAt").set(firebase.database.ServerValue.TIMESTAMP);
 })
 
 $(".codeTimeStop").on("click", function(){
@@ -10,4 +11,9 @@ var sessionsRef = firebase.database().ref("sessions");
 sessionsRef.push({
   stoppedAt: firebase.database.ServerValue.TIMESTAMP
 });
+database.ref("users/" + userId + "/stoppedAt").set(firebase.database.ServerValue.TIMESTAMP);
 })
+
+
+
+var userId = auth.uid;
