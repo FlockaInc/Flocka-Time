c77051b83cbdfd09a8f7f78cecc595e1b96bf6c2
bee25141 <bee25141@gmail.com>
2019-04-25 20:40:27 -0500

    Added sign in/sign out ui
    Added sign in/sign out ui

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+function signInDisplay(){
+    if (auth.uid = ""){
+        $(".signInButton").hide();
+        $(".signOutButton").show();
+        $(".welcomeContainer").show();
+        $("#welcomeElement").show();
+        $("#welcomeElement").text("Welcome, " + email);
+    }else {
+        $(".signInButton").show();
+        $(".signOutButton").hide();
+        $(".welcomeContainer").hide();
+        $("#welcomeElement").hide();
+    }
+}
+
+$(".signOutButton").on("click", function(){
+    firebase.auth().signOut()
+});
