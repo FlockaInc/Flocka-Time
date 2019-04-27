2a20f6713196576bd40064f32c1becad024e3c70
bee25141 <bee25141@gmail.com>
2019-04-27 12:06:25 -0500

    Added auth listener object

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,11 @@
 $(function() {
+  var app = {
+    authListener: notificationService.addObserver('AUTH_SIGNIN', this, handleSignIn)
+  }
+
+  function handleSignIn() {
+    console.log('user signed in');
+  }
   function signInDisplay() {
     if (auth.uid = "") {
       $(".signOutButton").removeClass("hide");
