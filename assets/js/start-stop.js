cdf46baa5535cbe4e7d880896a115d564c7b9b77
Alex Cahn <alexbcahn@gmail>
2019-04-27 10:41:02 -0500

    Update start-stop.js

--- assets/js/start-stop.js
+++ assets/js/start-stop.js
@@ -1,19 +1,19 @@
-$(".codeTimeStart").on("click", function(){
-var sessionsRef = firebase.database().ref("sessions");
-sessionsRef.push({
-  startedAt: firebase.database.ServerValue.TIMESTAMP
-});
-database.ref("users/" + userId + "/startedAt").set(firebase.database.ServerValue.TIMESTAMP);
+$(".codeTimeStart").on("click", function () {
+  var userId = auth.uid;
+  var sessionsRef = firebase.database().ref("sessions");
+  sessionsRef.push({
+    startedAt: firebase.database.ServerValue.TIMESTAMP
+  });
+  database.ref("codeTime/users/" + userId + "/startedAt").set(firebase.database.ServerValue.TIMESTAMP);
 })
 
-$(".codeTimeStop").on("click", function(){
-var sessionsRef = firebase.database().ref("sessions");
-sessionsRef.push({
-  stoppedAt: firebase.database.ServerValue.TIMESTAMP
-});
-database.ref("users/" + userId + "/stoppedAt").set(firebase.database.ServerValue.TIMESTAMP);
+$(".codeTimeStop").on("click", function () {
+  var userId = auth.uid;
+  console.log(userId)
+  var sessionsRef = firebase.database().ref("sessions");
+  sessionsRef.push({
+    stoppedAt: firebase.database.ServerValue.TIMESTAMP
+  });
+  database.ref("codeTime/users/" + userId + "/stoppedAt").set(firebase.database.ServerValue.TIMESTAMP);
 })
 
-
-
-var userId = auth.uid;
