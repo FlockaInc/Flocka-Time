a172b8e018da6a09333835a14f05c0737cbac27e
bee25141 <bee25141@gmail.com>
2019-04-30 17:39:43 -0500

    commit to pull master

---
+++ assets/js/graph.js
@@ -0,0 +1,41 @@
+  //D3 bar graph
+  var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+  var svgWidth = 900;
+  var svgHeight = 300;
+  var barPadding = 5;
+  var barWidth = (svgWidth / dataset.length);
+  var svg = d3.select('svg').attr("width", svgWidth).attr("height", svgHeight).attr("class", "bar-chart");
+  $(function () {
+      var barChart = svg.selectAll("rect")
+          .data(dataset)
+          .enter()
+          .append("rect")
+          .attr("y", function (d) {
+              return svgHeight - d
+          })
+          .attr("height", function (d) {
+              return d;
+          })
+          .attr("fill", "#282828")
+          .attr("width", barWidth - barPadding)
+          .attr("transform", function (d, i) {
+              var translate = [barWidth * i, 0];
+              return "translate(" + translate + ")";
+          });
+          debugger;
+      var text = svg.selectAll("text")
+          .data(dataset)
+          .enter()
+          .append("text")
+          .text(function (d) {
+              return d;
+          })
+          .attr("y", function (d, i) {
+              return svgHeight - d - 2;
+          })
+          .attr("x", function (d, i) {
+              return barWidth + i;
+          })
+          .attr("fill", "white");
+
+  })
