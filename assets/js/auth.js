6ddff3b3a713beb97918950510a7b4d015785c6f
Burke <burke1791@gmail.com>
2019-05-01 17:26:50 -0500

    deleted duplicate call to firebase create user function

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -10,27 +10,6 @@ var auth = {
 
   signUp: function (email, password) {
     // Call Firebase method to create user with email and password
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-
-      .then(function (data) {
-        var userId = data.user.uid;
-        var userEmail = data.user.email;
-        var emailObj = { email: userEmail };
-
-        // database.ref("users/" + uid + "/").set(emailObj);
-        data.createUser(emailObj);
-
-        console.log("User created: " + userId);
-        console.log("User created: " + userEmail); 
-      })
-
-      .catch(function (error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-
-        console.log("Error code: " + errorCode);
-        console.log("Error message: " + errorMessage);
-      });
     firebase.auth().createUserWithEmailAndPassword(email, password).then(function (user) {
       var userId = user.user.uid;
       auth.uid = userId;
