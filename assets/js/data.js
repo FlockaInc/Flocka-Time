3ffd19069cee3bdfe5c83cd5631870c898b59eea
Burke <burke1791@gmail.com>
2019-04-30 21:00:33 -0500

    merge master

--- assets/js/data.js
+++ assets/js/data.js
@@ -5,14 +5,6 @@ var data = {
       firebase.database().ref('/codeTime/users/' + auth.uid).once('value', function (codeTimeSnapshot) {
         var codeTime = codeTimeSnapshot.val();
         data.userCodeTime = codeTime;
-<<<<<<< HEAD
-        console.log(codeTime); 
-      });
-    }
-  },
-  createUser: function (email) {
-    database.ref("users/" + auth.uid + "/").set(email);
-=======
         console.log(codeTime);
       });
     }
@@ -22,7 +14,6 @@ var data = {
     firebase.database().ref("/users/" + uid + "/").set(emailObj).catch(function (error) {
       console.log(error.code);
     });
->>>>>>> 17295f802af446e84c8a920362bf22b4be6758ab
   },
   userStartTime: function () {
     var userId = auth.uid;
@@ -49,58 +40,6 @@ var data = {
   },
   createTimeInstance: function () {
     this.timeInstance.id = database.ref("time/users/" + auth.uid + "/").push({}).key;
-<<<<<<< HEAD
-
-    console.log("Current time instance: " + this.timeInstance.id);
-  },
-  updateTime: function (action) {
-    var timestamp = moment().format("YYYY-MM-DDTHH:mm:ss");
-    var timeObject = {};
-
-<<<<<<< HEAD
-    timeObject[action] = timestamp;
-
-    database.ref("time/users/" + auth.uid + "/" + this.timeInstance.id + "/").update(timeObject);
-  },
-  timeObject: {},
-  getTime: function () {
-    firebase.database().ref('time/users/' + auth.uid + "/" + this.timeInstance.id + "/")
-      .once('value', function (snapshot) {
-        data.timeObject = snapshot.val();
-
-        // Must use notification service since to trigger synchronous event
-        notificationService.postNotification('TIME_FETCHED', null);
-      });
-  },
-  totalTime: "",
-  calculateTotalTime: function () {
-    var start = moment(this.timeObject.start);
-    var stop = moment(this.timeObject.stop);
-
-    this.totalTime = stop.diff(start, "minutes");
-    console.log(stop.diff(start, "seconds"));
-  }
-=======
-        timeObject[action] = timestamp;
-        
-        database.ref("time/users/" + auth.uid + "/" + this.timeInstance.id + "/").update(timeObject);
-    },
-    timeObject: {},
-    getTime: function() {
-        firebase.database().ref('time/users/' + auth.uid + "/")
-            .once('value', function (snapshot) {
-                data.timeObject = snapshot.val();
-                
-                // Must use notification service since to trigger synchronous event
-                notificationService.postNotification('TIME_FETCHED', null);
-        });
-    },
-    totalTime: "",
-    calculateTotalTime: function() {
-        var keys = Object.keys(this.timeObject);
-        var i;
-        var j;
-=======
 
     console.log("Current time instance: " + this.timeInstance.id);
   },
@@ -117,7 +56,6 @@ var data = {
     firebase.database().ref('time/users/' + auth.uid + "/")
       .once('value', function (snapshot) {
         data.timeObject = snapshot.val();
->>>>>>> 17295f802af446e84c8a920362bf22b4be6758ab
 
         // Must use notification service since to trigger synchronous event
         notificationService.postNotification('TIME_FETCHED', null);
@@ -157,14 +95,6 @@ var data = {
     stop = moment(stop);
 
     timeDiff = stop.diff(start, "seconds");
-
-<<<<<<< HEAD
-        return timeDiff;
-    }
->>>>>>> 44235b6643d440949b72411e16354b9f851b85de
-}
-=======
     return timeDiff;
   }
-};
->>>>>>> 17295f802af446e84c8a920362bf22b4be6758ab
+}
